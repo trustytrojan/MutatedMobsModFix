2675ee1137b032c1944e7bfd3f54926d46d270ee
trustytrojan <87675609+trustytrojan@users.noreply.github.com>
2025-11-26 16:14:21 -0500

    change mod metadata

--- src/main/java/mmm/MainClass.java
+++ src/main/java/mmm/MainClass.java
@@ -12,12 +12,9 @@ import net.minecraftforge.common.*;
 import mmm.eventHandler.*;
 import net.minecraftforge.fml.common.event.*;
 
-@Mod(modid = "mmm", version = "1.0.8", acceptedMinecraftVersions = "[1.12]")
+@Mod(modid = "mmm", version = "1.0.9", name = "Mutated Mobs Mod Fix", acceptedMinecraftVersions = "[1.12]")
 public class MainClass
 {
-    public static final String MODID = "mmm";
-    public static final String VERSION = "1.0.8";
-    public static final String NAME = "Mutated Mobs Mod";
     @SidedProxy(clientSide = "mmm.client.proxy.ClientProxy", serverSide = "mmm.common.proxy.CommonProxy")
     public static CommonProxy proxy;
     @Mod.Instance("mmm")
